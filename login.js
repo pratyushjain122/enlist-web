a13b0adeada45f30dfb7d1e5de5bedde7f4397e4
Rahul Mansharamani <rahul.mansharamani.btech2019@sitpune.edu.in>
2020-09-19 20:38:16 +0530

    login with google done

--- login.js
+++ login.js
@@ -21,11 +21,11 @@ var uiConfig = {
     signInSuccessUrl: 'home.html',
     signInOptions: [
       // Leave the lines as is for the providers you want to offer your users.
-    //   firebase.auth.GoogleAuthProvider.PROVIDER_ID
+       firebase.auth.GoogleAuthProvider.PROVIDER_ID
     //   firebase.auth.FacebookAuthProvider.PROVIDER_ID,
     //   firebase.auth.TwitterAuthProvider.PROVIDER_ID,
     //   firebase.auth.GithubAuthProvider.PROVIDER_ID,
-      firebase.auth.EmailAuthProvider.PROVIDER_ID
+    //  firebase.auth.EmailAuthProvider.PROVIDER_ID
     //   firebase.auth.PhoneAuthProvider.PROVIDER_ID
     ],
     // Terms of service url.
