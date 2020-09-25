a9477086b15c2d536bd0bd489bdecc7b43f5104b
sudhanshu <sudhanshu.dpandey@gmail.com>
2020-09-25 20:51:13 +0530

    Sync Test Module Added

---
+++ Data_Analysis/Sync Module/Sync2.py
@@ -0,0 +1,71 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import  db
+import random
+
+cred=credentials.Certificate("firabase_enlist_sdk.json")
+
+firebase_admin.initialize_app(cred,{
+
+    'databaseURL':"https://enlist-duo.firebaseio.com/"
+
+})
+
+user='bFdu73bKCrWUZISAz01gXMR46qE2'#'y920c43argQfQw6SM3CtlIUyyUC2'
+
+
+def Create(user,Class):
+
+    ref=db.reference("To-Do-List/{}/{}".format(user, Class))
+    #print(ref.get())
+
+    val=random.randint(99999999,1000000000)
+    Descrip=input("Discription : ")
+    Tital=input("Title : ")
+    chile_ref=ref.child("Task-{}".format(val))
+    chile_ref.set({
+        #"Task-{}".format(val):
+
+                "description": Descrip, "key": val, "title": Tital
+
+    })
+
+    #Sync(user,Class,val)
+
+
+
+def Sync(Id,Batch,Key):
+
+    Key="Task-{}".format(Key)
+    main_data=db.reference("To-Do-List/").get()
+   # print("MAIN DATA IS : ")
+   # print(main_data)
+    #print("User List is : ")
+    user_list = list(main_data.keys())
+    #print(user_list)
+
+    #print("Values: ",Id," ",Batch," ",Key)
+    Add_Data = main_data[Id][Batch][Key]
+    #print(Add_Data)
+
+    for user in user_list:
+        if user!=Id:
+            #print("ID IS : ",user)
+            key = list(main_data[user].keys())
+            #print("\n###\n ",key)
+            for batch in key:
+                if Batch==batch:
+                    #print("Batch : ",batch,"  user :",user)
+                    ref=db.reference("To-Do-List/{}/{}".format(user,Batch))
+                    val = random.randint(99999999, 1000000000)
+                    child_ref = ref.child("Task-{}".format(val))
+                    child_ref.set(Add_Data)
+
+
+#Sync(user,Class,"Task-1188341295")
+'''
+while(True):
+    Class = input("Enter Class You want to add in : ")
+    print("\n\n")
+    Create()
+'''
