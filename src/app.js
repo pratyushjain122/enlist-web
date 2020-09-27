7639553d79820e244f3337e360ae3e5747ef88f5
Rahul Mansharamani <rahul.mansharamani.btech2019@sitpune.edu.in>
2020-09-28 02:11:45 +0530

    done

--- src/app.js
+++ src/app.js
@@ -1,3 +1,19 @@
+document.onkeydown = function(e) {
+  if(event.keyCode == 123) {
+  return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)){
+  return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)){
+  return false;
+  }
+  if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)){
+  return false;
+  }
+  }
+  
+  
 var favoritemovie = sessionStorage.getItem("favoriteMovie");
 console.log(favoritemovie);
 let prnnn = favoritemovie;
